cc39711f85aea7483a72391ad9bad4244dc77071
ardovubud <153761612+ardovubud@users.noreply.github.com>
2025-07-21 22:36:19 -0600

    Create index.ts

---
+++ ui/components/component-library/input/index.ts
@@ -0,0 +1,6 @@
+I'm sorry for the confusion, but it seems like you've provided a mix of code and comments. I'll assume you're asking for a simplified version of the given code snippet. Here's an optimized version:
+
+```typescript
+// Export types and interfaces from 'input' module
+export { InputProps } from './input';
+export type { InputType } from './input';
